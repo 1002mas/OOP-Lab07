ee581762799f1d3ca620c9bb497b67347528c76e
Carafassi Samuele <samuele.carafassi@studio.unibo.it>
2021-11-28 16:51:54 +0100

    revisioning: nesting2

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,40 +1,35 @@
 package it.unibo.oop.lab.nesting2;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class OneListAcceptable<T> implements Acceptable<T> {
     private final List<T> list;
 
     public OneListAcceptable(List<T> elements) {
-	list = new ArrayList<>();
-	for(T elem:elements) {
-	    list.add(elem);
-	}
+        this.list = elements;
     }
 
     @Override
     public Acceptor<T> acceptor() {
-	return new Acceptor<T>() {
-	    private final List<T> erasableLista= list;
-	    private final List<T> lista = List.copyOf(erasableLista);
+        return new Acceptor<T>() {
+            private final Iterator<T> iterator = list.iterator();
 
-	    @Override
-	    public void accept(T newElement) throws ElementNotAcceptedException {
-		if (!lista.contains(newElement)) {
-		    throw new ElementNotAcceptedException(newElement);
-		}
-		erasableLista.remove(newElement);
-	    }
+            @Override
+            public void accept(T newElement) throws ElementNotAcceptedException {
+                if (!iterator.hasNext() || !newElement.equals(iterator.next())) {
+                    throw new ElementNotAcceptedException(newElement);
+                }
+            }
 
-	    @Override
-	    public void end() throws EndNotAcceptedException {
-		if (!erasableLista.isEmpty()) {
-		    throw new EndNotAcceptedException();
-		}
-	    }
+            @Override
+            public void end() throws EndNotAcceptedException {
+                if (iterator.hasNext()) {
+                    throw new EndNotAcceptedException();
+                }
+            }
 
-	};
+        };
     }
 
 }
